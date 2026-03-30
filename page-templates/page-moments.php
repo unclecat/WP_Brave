48b8d7ca2eae661bd611852759bdde79ace86b8c
Shawn <shawn@ShawndeMac-mini.lan>
2026-03-30 18:48:19 +0800

    Release v0.1.20 - Fix moment detail page link

--- page-templates/page-moments.php
+++ page-templates/page-moments.php
@@ -68,54 +68,56 @@ rsort($years);
                         $related_memory = get_post_meta($moment->ID, '_related_memory', true);
                         $has_thumbnail = has_post_thumbnail($moment->ID);
                     ?>
-                        <div class="timeline-item">
-                            <div class="timeline-dot"></div>
-                            <span class="timeline-date"><?php echo esc_html($meet_date); ?></span>
-                            <div class="timeline-content">
-                                <div class="timeline-header">
-                                    <h4 class="timeline-title">
-                                        <?php echo esc_html($moment->post_title); ?>
-                                    </h4>
-                                    <?php if ($mood) : ?>
-                                        <span class="timeline-mood" title="<?php echo esc_attr(brave_get_mood_text($mood)); ?>">
-                                            <?php echo brave_get_mood_emoji($mood); ?>
-                                        </span>
+                        <a href="<?php echo esc_url(get_permalink($moment->ID)); ?>" class="timeline-item-link">
+                            <div class="timeline-item">
+                                <div class="timeline-dot"></div>
+                                <span class="timeline-date"><?php echo esc_html($meet_date); ?></span>
+                                <div class="timeline-content">
+                                    <div class="timeline-header">
+                                        <h4 class="timeline-title">
+                                            <?php echo esc_html($moment->post_title); ?>
+                                        </h4>
+                                        <?php if ($mood) : ?>
+                                            <span class="timeline-mood" title="<?php echo esc_attr(brave_get_mood_text($mood)); ?>">
+                                                <?php echo brave_get_mood_emoji($mood); ?>
+                                            </span>
+                                        <?php endif; ?>
+                                    </div>
+                                    
+                                    <div class="timeline-text">
+                                        <?php echo wpautop(wp_trim_words($moment->post_content, 60)); ?>
+                                    </div>
+                                    
+                                    <?php if ($has_thumbnail || $related_memory) : ?>
+                                        <div class="timeline-images">
+                                            <?php if ($has_thumbnail) : ?>
+                                                <?php echo get_the_post_thumbnail($moment->ID, 'thumbnail', array('class' => 'timeline-image')); ?>
+                                            <?php endif; ?>
+                                            
+                                            <?php if ($related_memory) : 
+                                                $memory_photos = brave_get_memory_photos($related_memory, 'thumbnail');
+                                                $show_photos = array_slice($memory_photos, 0, $has_thumbnail ? 3 : 4);
+                                                foreach ($show_photos as $photo) : ?>
+                                                    <img src="<?php echo esc_url($photo['url']); ?>" alt="" class="timeline-image">
+                                                <?php endforeach; 
+                                                $remaining = count($memory_photos) - count($show_photos);
+                                                if ($remaining > 0) : ?>
+                                                    <span class="timeline-image-more">
+                                                        +<?php echo $remaining; ?>
+                                                    </span>
+                                                <?php endif; ?>
+                                            <?php endif; ?>
+                                        </div>
+                                    <?php endif; ?>
+                                    
+                                    <?php if ($location) : ?>
+                                        <div class="timeline-meta">
+                                            <span class="timeline-location"><?php echo esc_html($location); ?></span>
+                                        </div>
                                     <?php endif; ?>
                                 </div>
-                                
-                                <div class="timeline-text">
-                                    <?php echo wpautop(wp_trim_words($moment->post_content, 60)); ?>
-                                </div>
-                                
-                                <?php if ($has_thumbnail || $related_memory) : ?>
-                                    <div class="timeline-images">
-                                        <?php if ($has_thumbnail) : ?>
-                                            <?php echo get_the_post_thumbnail($moment->ID, 'thumbnail', array('class' => 'timeline-image')); ?>
-                                        <?php endif; ?>
-                                        
-                                        <?php if ($related_memory) : 
-                                            $memory_photos = brave_get_memory_photos($related_memory, 'thumbnail');
-                                            $show_photos = array_slice($memory_photos, 0, $has_thumbnail ? 3 : 4);
-                                            foreach ($show_photos as $photo) : ?>
-                                                <img src="<?php echo esc_url($photo['url']); ?>" alt="" class="timeline-image">
-                                            <?php endforeach; 
-                                            $remaining = count($memory_photos) - count($show_photos);
-                                            if ($remaining > 0) : ?>
-                                                <a href="<?php echo esc_url(get_permalink($related_memory)); ?>" class="timeline-image-more">
-                                                    +<?php echo $remaining; ?>
-                                                </a>
-                                            <?php endif; ?>
-                                        <?php endif; ?>
-                                    </div>
-                                <?php endif; ?>
-                                
-                                <?php if ($location) : ?>
-                                    <div class="timeline-meta">
-                                        <span class="timeline-location"><?php echo esc_html($location); ?></span>
-                                    </div>
-                                <?php endif; ?>
                             </div>
-                        </div>
+                        </a>
                     <?php endforeach; ?>
                 </div>
             </div>
