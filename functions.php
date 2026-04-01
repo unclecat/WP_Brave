463300252e961e4daaac472d76aeb7ade60919a0
Shawn <shawn@ShawndeMac-mini.lan>
2026-04-01 15:50:53 +0800

    v0.6.11: 修复恋爱清单和首页深色模式

--- functions.php
+++ functions.php
@@ -6,7 +6,7 @@
  */
 
 // 定义常量
-define('BRAVE_VERSION', '0.6.9');
+define('BRAVE_VERSION', '0.6.11');
 define('BRAVE_DIR', get_template_directory());
 define('BRAVE_URI', get_template_directory_uri());
 
