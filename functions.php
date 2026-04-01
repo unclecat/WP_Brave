fb632aae0fec2f65e89722d68b2e0133b6fb312c
Shawn <shawn@ShawndeMac-mini.lan>
2026-04-01 16:51:59 +0800

    v0.6.20: 彻底修复Hero背景图显示问题

--- functions.php
+++ functions.php
@@ -6,7 +6,7 @@
  */
 
 // 定义常量
-define('BRAVE_VERSION', '0.6.19');
+define('BRAVE_VERSION', '0.6.20');
 define('BRAVE_DIR', get_template_directory());
 define('BRAVE_URI', get_template_directory_uri());
 
