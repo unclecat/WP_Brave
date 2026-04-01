027b21d9a49da74f55e1d20e5a8d8559a325dc1d
Shawn <shawn@ShawndeMac-mini.lan>
2026-04-01 15:21:48 +0800

    v0.6.7: 导航栏样式优化，增加间距、优化字体、解决重合问题

--- functions.php
+++ functions.php
@@ -6,7 +6,7 @@
  */
 
 // 定义常量
-define('BRAVE_VERSION', '0.6.6');
+define('BRAVE_VERSION', '0.6.7');
 define('BRAVE_DIR', get_template_directory());
 define('BRAVE_URI', get_template_directory_uri());
 
