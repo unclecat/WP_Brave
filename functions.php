30fcd2eb162114c6dce0d12015616046b5205ea4
Shawn <shawn@ShawndeMac-mini.lan>
2026-04-01 14:25:22 +0800

    v0.6.2: 祝福留言页面深色模式支持

--- functions.php
+++ functions.php
@@ -6,7 +6,7 @@
  */
 
 // 定义常量
-define('BRAVE_VERSION', '0.6.1');
+define('BRAVE_VERSION', '0.6.2');
 define('BRAVE_DIR', get_template_directory());
 define('BRAVE_URI', get_template_directory_uri());
 
